a5ea9cabb1fc69a8f46cda3a5a8a7f998073642c
Tony Song <tony99019@gmail.com>
2017-03-23 12:15:49 -0500

    Delete KittenComponent
    Issue: #5
    
    We deleted an unnecessary component from the boilerplate code.

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -5,7 +5,6 @@ import { HttpModule, JsonpModule } from '@angular/http';
 import { AppComponent }         from './app/app.component';
 import { NavbarComponent } from './app/navbar/navbar.component';
 import { HomeComponent} from './app/home/home.component';
-import { KittensComponent }   from './app/kittens/kittens.component';
 import { UserListComponent } from './app/users/user-list.component';
 import { UserListService } from './app/users/user-list.service';
 import { PlantListComponent } from './app/plants/plant-list.component';
@@ -27,7 +26,6 @@ import { PipeModule } from './pipe.module';
     ],
     declarations: [
         AppComponent,
-        KittensComponent,
         HomeComponent,
         NavbarComponent,
         UserListComponent,
